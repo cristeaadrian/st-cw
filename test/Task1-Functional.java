4d275fafec21ce2a8da19c15df3f340e88d494d0
Adrian Cristea <cristea.adrian@icloud.com>
2018-02-22 18:56:48 +0000

    added files that have to be submitted

---
+++ test/Task1-Functional.java
@@ -0,0 +1,17 @@
+/*
+Software Testing Coursework 1 - Part 1: Functional Testing (20 Marks)
+Collaborators: Adrian Cristea (s1449640) & Andra Zaharia (s1402967)
+
+Specification:
+The repository provides the implementation as a JAR file, ST_Coursework.jar, so you can execute your JUnit tests
+and observe test results. The specification is described in detail, with helpful examples where necessary in the
+Specifications.pdf file.
+
+Functional testing is a black box testing technique, so use the specification file to derive tests and not the source code.
+The jar file under the jar directory can be used to execute the tests derived from the specification.
+We have also provided a sample JUnit test case, TemplateEngineTest.java file, to illustrate a typical test case for
+the implementation in ST_Coursework.jar.
+
+In giving a grade for this part of the practical I will take into account the performance of your test set
+on a collection of variants/mutants of the specification.
+ */
