17ee9f06b93fa66b54d41a6819843e9374324246
Adrian Cristea <cristea.adrian@icloud.com>
2018-02-23 13:19:05 +0000

    added first two tests for the delete() function

--- src/Task1_Functional.java
+++ src/Task1_Functional.java
@@ -1,21 +1,3 @@
-package test;/*
-Software Testing Coursework 1 - Part 1: Functional Testing (20 Marks)
-Collaborators: Adrian Cristea (s1449640) & Andra Zaharia (s1402967)
-
-Specification:
-The repository provides the implementation as a JAR file, ST_Coursework.jar, so you can execute your JUnit tests
-and observe test results. The specification is described in detail, with helpful examples where necessary in the
-Specifications.pdf file.
-
-Functional testing is a black box testing technique, so use the specification file to derive tests and not the source code.
-The jar file under the jar directory can be used to execute the tests derived from the specification.
-We have also provided a sample JUnit test case, test.TemplateEngineTest.java file, to illustrate a typical test case for
-the implementation in ST_Coursework.jar.
-
-In giving a grade for this part of the practical I will take into account the performance of your test set
-on a collection of variants/mutants of the specification.
- */
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
@@ -66,6 +48,18 @@ public class Task1_Functional {
          */
         map.store("name", "Adam");
         map.store("surname", "Dykes");
+    }
+
+    @Test (expected=RuntimeException.class)
+    public void deleteNull() {
+        map.store("name", "Adam");
+        map.delete(null);
+    }
+
+    @Test (expected=RuntimeException.class)
+    public void deleteEmpty() {
+        map.store("name", "Adam");
+        map.delete("");
     }
 
 }
--- src/TemplateEngineTest.java
+++ src/TemplateEngineTest.java
@@ -1,5 +1,3 @@
-package test;
-
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
