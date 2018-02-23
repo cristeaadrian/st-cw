1580461b6231179bd8432299b8242c1e6acb01a6
Andra Zaharia <andrazaharia@protonmail.com>
2018-02-23 12:36:47 +0000

    Added first 3 tests for EntryMap.store()

--- src/test/Task1_Functional.java
+++ src/test/Task1_Functional.java
@@ -1,4 +1,4 @@
-/*
+package test;/*
 Software Testing Coursework 1 - Part 1: Functional Testing (20 Marks)
 Collaborators: Adrian Cristea (s1449640) & Andra Zaharia (s1402967)
 
@@ -16,3 +16,61 @@ In giving a grade for this part of the practical I will take into account the pe
 on a collection of variants/mutants of the specification.
  */
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Before;
+import org.junit.Test;
+import st.EntryMap;
+import st.SimpleTemplateEngine;
+import st.TemplateEngine;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Task1_Functional {
+
+    private EntryMap map;
+
+    private TemplateEngine engine;
+
+    private SimpleTemplateEngine simpleEngine;
+
+    @Before
+    public void setUp() throws Exception {
+        map = new EntryMap();
+        engine = new TemplateEngine();
+        simpleEngine = new SimpleTemplateEngine();
+    }
+
+    //EntryMap.store() tests
+    @Test (expected=RuntimeException.class)
+    public void Test1() {
+        map.store(null, "Adam");
+    }
+
+    @Test (expected=RuntimeException.class)
+    public void Test2() {
+        map.store("", "Adam");
+    }
+
+    @Test (expected=RuntimeException.class)
+    public void Test3() {
+        map.store("name", null);
+    }
+
+    @Test ()
+    public void Test6() {
+        map.store("name", "Adam");
+        ArrayList<EntryMap.Entry> entries = map.getEntries();
+        for (Map.Entry entr)
+    }
+
+
+//    public void Test4() {
+//        EntryMap map_2 = new EntryMap();
+//        assertSame(map_2, map);
+//        map_2.store("name", null);
+//    }
+
+}
