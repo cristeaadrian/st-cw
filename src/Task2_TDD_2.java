f24f121feab4e240ddc3fa9a2f288d8889a4bb13
Adrian Cristea <cristea.adrian@icloud.com>
2018-03-10 17:31:57 +0000

    fixed the last test

--- src/Task2_TDD_2.java
+++ src/Task2_TDD_2.java
@@ -88,11 +88,11 @@ public class Task2_TDD_2 {
         map.store("base_year", "1990");
         Integer matchingMode = TemplateEngine.DEFAULT;
         String result = engine.evaluate("I was born in ${year}", map, matchingMode);
-        assertEquals("I was born in " + Integer.toString(currentYear), result);
+        assertEquals("I was born in 1990", result);
 
         map.update("year", "0 years ago");
         result = engine.evaluate("I was born in ${year}", map, matchingMode);
-        assertEquals("I was born in " + Integer.toString(currentYear), result);
+        assertEquals("I was born in 1990", result);
     }
 
     /* Spec 3:
