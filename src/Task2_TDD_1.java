ff5be1c5508f801e3b2d6ec9c00518704e489257
Adrian Cristea <cristea.adrian@icloud.com>
2018-03-10 16:23:57 +0000

    added first three examples

--- src/Task2_TDD_1.java
+++ src/Task2_TDD_1.java
@@ -1,2 +1,118 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import st.EntryMap;
+import st.SimpleTemplateEngine;
+import st.TemplateEngine;
+
+import java.util.Calendar;
+
 public class Task2_TDD_1 {
+
+    private EntryMap map;
+
+    private TemplateEngine engine;
+
+    private SimpleTemplateEngine simpleEngine;
+
+    @Before
+    public void setUp() {
+        map = new EntryMap();
+        engine = new TemplateEngine();
+        simpleEngine = new SimpleTemplateEngine();
+    }
+
+    /* Spec 1:
+     * If X is not a number or a negative number then there should be no special treatment of the template ${year}.
+     */
+    @Test
+    public void notNumber() {
+
+    }
+
+    @Test
+    public void negativeNumber() {
+
+    }
+
+    /* Spec 2:
+     * If X is zero then the replacement value should be the current year.
+     */
+
+    @Test
+    public void valueIsZero() {
+
+    }
+
+    /* Spec 3:
+     * If the EntryMap class used contains the template ${base year} with a positive integer as a replacement value,
+     * then this number should be used to calculate the replacement value of ${year} instead of the current year.
+     * In all other cases, the referenced year is the current year.
+     */
+
+    @Test
+    public void containsBaseYearPositive() {
+
+    }
+
+    @Test
+    public void containsBaseYearNegative() {
+
+    }
+
+    @Test
+    public void containsBaseYearEmpty() {
+
+    }
+
+    @Test
+    public void containsBaseYearNull() {
+
+    }
+
+    @Test
+    public void containsBaseYearOther() {
+
+    }
+
+    /* Check that all usual input values work correctly
+     * Check special cases
+     */
+
+    @Test
+    public void exampleOne() {
+        Calendar now = Calendar.getInstance();
+        Integer currentYear = now.get(Calendar.YEAR);
+
+        map.store("year", "5 years ago");
+        Integer matchingMode = TemplateEngine.DEFAULT;
+        String result = engine.evaluate("My team won the cup in ${year}", map, matchingMode);
+        assertEquals("My team won the cup in " + Integer.toString(currentYear + 2), result);
+    }
+
+    @Test
+    public void exampleTwo() {
+        map.store("year", "in 2 years");
+        map.store("base_year", "1990");
+        Integer matchingMode = TemplateEngine.DEFAULT;
+        String result = engine.evaluate("I was born in ${year}", map, matchingMode);
+        assertEquals("I was born in 1992", result);
+    }
+
+    @Test
+    public void exampleThree() {
+        map.store("year", "in 2 years");
+        map.store("base_year", "1990");
+        Integer matchingMode = TemplateEngine.DEFAULT;
+        String result = engine.evaluate("I was born in ${year} ${base year}", map, matchingMode);
+        assertEquals("I was born in 1992 1990", result);
+    }
+
+    @Test
+    public void allValidInputs() {
+        
+    }
+
 }
