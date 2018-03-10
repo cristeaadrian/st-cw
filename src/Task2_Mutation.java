e8ae3d20cd27878f566ad6fdc1c0013b520e9796
Adrian Cristea <cristea.adrian@icloud.com>
2018-03-10 14:27:46 +0000

    created file for the first 5 mutations, in task 2 of the coursework

---
+++ src/Task2_Mutation.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import st.EntryMap;
+import st.SimpleTemplateEngine;
+import st.TemplateEngine;
+
+public class Task2_Mutation {
+    private EntryMap map;
+
+    private TemplateEngine engine;
+
+    private SimpleTemplateEngine simpleEngine;
+
+    @Before
+    public void setUp() {
+        map = new EntryMap();
+        engine = new TemplateEngine();
+        simpleEngine = new SimpleTemplateEngine();
+    }
+
+    @Test
+    public void sample() {
+
+    }
+}
